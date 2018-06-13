0f0fa28cea8a8472863fbc9e0831cd5fa6cffe5c
Aye Thandar Aung <ayethandaraung588@gmail.com>
2018-06-13 12:02:37 +0530

    Lesson2

--- hello.py
+++ hello.py
@@ -1,15 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def home():
-    return '''
-    <html>
-    <head><title> This is my flask app (/title></head>
-    <body><h1> Hello From Flask </h1>
-    </html>
-    '''
+    return render_template("templ.html")
 if __name__ == "__main__":
     app.run(debug=True)
